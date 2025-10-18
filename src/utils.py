aecd42f7eb08769c355525e3a4793beb7de8a5b9
Mateusz Bielowka <mbielowka@student.agh.edu.pl>
2025-10-18 13:29:16 +0200

    Added documents, complete read_files(), tokenize_text(), create_frequency_table() from utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,23 +1,43 @@
+import re
+import os
 import pandas as pd
 from collections import Counter
 
 
-def read_files(filepath):
+def read_files(path):
     # TODO: iteruje po plikach w folderze zwraca liste krotek (nazwapliku,tekst)
-    files = []
+    fileslist = []
 
-    return files
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name.endswith((".txt")) and name != "blacklist.txt":
+                filepath = os.path.join(root, name)
+                with open(filepath, mode='r', encoding='utf-8', errors='ignore') as f:
+                    content = f.read()
+                pair = (filepath, content)
+                fileslist.append(pair)
+
+    return fileslist
 
 
 def tokenize_text(text):
     # TODO: dzieli tekst na zdania, usuwa zbędne znaki, zwraca liste wyrazów
+    text = re.sub(r'\b[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ]{1,3}\.\b', '', text)  # simple abbreviations
+    text = re.sub(r'\d+', '', text)  # digits
+    text = re.sub(r'[^a-zA-ZáéíóúãõâêîôûàèìòùçÁÉÍÓÚÃÕÂÊÎÔÛÀÈÌÒÙÇ\s]', '', text)  # only english and portuguese letters
+    text = re.sub(r'\s+', ' ', text).strip()  # multiple spaces
     text = text.lower()
+    list_of_words = text.split()
+    return list_of_words
+
 
 
 def create_frequency_table(tokenslist):
     # TODO: tworzy dataframe z polami słowo, częstotliwość z listy tokenów
     frequencies = Counter(tokenslist)
-    df = pd.DataFrame(frequencies, columns=["word", "freq"])
+    df = pd.DataFrame(frequencies.items(), columns=["word", "freq"])
+    df = df.sort_values(by="freq", ascending=False).reset_index(drop=True)
+    return df
 
 
 def check_zipf_law(df):
--- zipf_calculations.py
+++ zipf_calculations.py
@@ -1,20 +1,27 @@
+import os
+
+import numpy as np
+
 from src.utils import (
     read_files,
     tokenize_text,
     create_frequency_table,
     check_zipf_law,
     create_concurence_graph
-    )
+)
+
 
 def main():
     # TODO: wczytać pliki funkcją, zapisać wyniki (tabelki, wyznaczniki) do plików json bo na tym polega podobno to całe API
     # TODO: tutaj też możemy tworzyć i zapisywać pliki z wykresików do folderu plots
     file_list = read_files("Data")
-
-
-
-
-
+    list_of_texts = [x[1] for x in file_list]
+    list_of_tokens = [tokenize_text(text) for text in list_of_texts]
+    flat_list = []
+    for sublist in list_of_tokens:
+        for token in sublist:
+            flat_list.append(token)
+    print(len(flat_list))
 
 
 
